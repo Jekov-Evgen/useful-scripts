e9ff0847ddc4836ec0d2383a28f5a16c4c76c651
Jekov-Evgen <jena1245481te@gmail.com>
2024-11-29 00:55:49 +0200

    refactoring

--- system_monitoring.py
+++ system_monitoring.py
@@ -5,13 +5,13 @@ cpu = psutil.cpu_percent(interval=1, percpu=True)
 gpu = GPUtil.getGPUs()
 
 print("Нагрузка на процессор: ", round(sum(cpu)))
-print("Данные видеокарты видеокарты: ")
+print("Данные видеокарты: ")
 
 for i in gpu:
     print("Температура видеокарты: ", i.temperature)
     
     if i.load == 0.0:
-        print("Ваша дискретаная видеокартв не рабоатет")
+        print("Ваша дискретаная видеокарта не рабоатет")
         break
     
     print("Нагрузка видеокарты: ", i.load)
